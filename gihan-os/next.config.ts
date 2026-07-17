b5cc27c24cf295a50d99bcc4d5c19c38080d5f57
Gihan <mastergba2001@gmail.com>
2026-07-18 01:18:43 +0530

    bug fixed #2

--- gihan-os/next.config.ts
+++ gihan-os/next.config.ts
@@ -3,6 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: false,
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
